eb24190acf967ce902d3fa0d0ab2712a5973b511
RoussKS <rkarafylla@yahoo.gr>
2018-12-20 22:02:09 +0000

    finalised init form of Interface

--- src/Interfaces/AdapterInterface.php
+++ src/Interfaces/AdapterInterface.php
@@ -30,9 +30,11 @@ interface AdapterInterface
      * Set the financial year's start date.
      *
      * Expects either string ISO-8601 format 'YYYY-MM-DD'
-     * or a date object, same object instance as the adapter's
+     * or a date object, same object instance as the adapter's that extends the DateTimeInterface
      *
-     * @param  mixed $date
+     * Throws an exception if FyEndDate is already set.
+     *
+     * @param  string|\DateTimeInterface $date
      *
      * @return void
      *
@@ -51,29 +53,146 @@ interface AdapterInterface
      * Set the financial year's end date.
      *
      * Expects either string ISO-8601 format 'YYYY-MM-DD'
-     * or a date object, same object instance as the adapter's
-     * or null where the adapter sets itself according the start date.
+     * or a date object, same object instance as the adapter's that extends the DateTimeInterface
      *
-     * @param  mixed $date
+     * Throws an exception if FyEndDate is already set.
+     *
+     * @param  string|\DateTimeInterface $date
      * @param  bool $fiftyThreeWeeks // applicable to Financial Year TypeEnum 'business'
      *
      * @return void
      */
-    public function setFyEndDate($date = null, $fiftyThreeWeeks = false);
+    public function setFyEndDate($date, $fiftyThreeWeeks = false);
 
     /**
+     * @param  \DateTimeInterface $startDate
+     * @param  \DateTimeInterface $endDate
+     *
      * @return int
      *
      * @throws ConfigException
      */
-    public function getPeriodId();
+    public function getPeriodIdByDateRange(\DateTimeInterface $startDate, \DateTimeInterface $endDate);
 
     /**
+     * @param  \DateTimeInterface $startDate
+     * @param  \DateTimeInterface $endDate
+     *
      * @return int
      *
      * @throws ConfigException
      */
-    public function getBusinessWeekId();
+    public function getBusinessWeekIdByDateRange(\DateTimeInterface $startDate, \DateTimeInterface $endDate);
+
+    /**
+     * Get the date range of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getPeriodById(int $id);
+
+    /**
+     * Get the date range of the business week with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getBusinessWeekById(int $id);
+
+    /**
+     * Get the first date of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \DateTimeInterface
+     *
+     * @throws ConfigException
+     */
+    public function getFirstDateOfPeriodById(int $id);
+
+    /**
+     * Get the last date of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \DateTimeInterface
+     *
+     * @throws ConfigException
+     */
+    public function getLastDateOfPeriodById(int $id);
+
+    /**
+     * Get the first date of the business week with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \DateTimeInterface
+     *
+     * @throws ConfigException
+     */
+    public function getFirstDateOfBusinessWeekById(int $id);
+
+    /**
+     * Get the last date of the business week with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \DateTimeInterface
+     *
+     * @throws ConfigException
+     */
+    public function getLastDateOfBusinessWeekById(int $id);
+
+    /**
+     * Get the date range of the first business week of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getFirstBusinessWeekByPeriodId(int $id);
+
+    /**
+     * Get the date range of the second business week of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getSecondBusinessWeekByPeriodId(int $id);
+
+    /**
+     * Get the date range of the third business week of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getThirdBusinessWeekOfPeriodId(int $id);
+
+    /**
+     * Get the date range of the fourth business week of the period with the given id.
+     *
+     * @param  int $id
+     *
+     * @return \Traversable
+     *
+     * @throws ConfigException
+     */
+    public function getFourthBusinessWeekByPeriodId(int $id);
 
     /**
      * @return void
