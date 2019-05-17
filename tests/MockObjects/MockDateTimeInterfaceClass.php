729c716f59c32aa540b4d803f063e4c1bbcdcbc3
RoussKS <7203382+RoussKS@users.noreply.github.com>
2019-05-17 19:35:59 +0100

    docblock on mock object

--- tests/MockObjects/MockDateTimeInterfaceClass.php
+++ tests/MockObjects/MockDateTimeInterfaceClass.php
@@ -2,6 +2,14 @@
 
 namespace RoussKS\FinancialYear\Tests\MockObjects;
 
+/**
+ * Class MockDateTimeInterfaceClass
+ *
+ * Mock class to use with available factories.
+ * As we are checking for the exact fully namespaced class name, this should fail.
+ *
+ * @package RoussKS\FinancialYear\Tests\MockObjects
+ */
 class MockDateTimeInterfaceClass extends \DateTime
 {
 
