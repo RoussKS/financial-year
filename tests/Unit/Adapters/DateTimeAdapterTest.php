47c7e15827843002532f11df6a3ab453508f5f2c
RoussKS <rkarafylla@gmail.com>
2019-05-25 20:48:34 +0100

    assert fy start and end dates getters. though end is already tested

--- tests/Unit/Adapters/DateTimeAdapterTest.php
+++ tests/Unit/Adapters/DateTimeAdapterTest.php
@@ -2,6 +2,7 @@
 
 namespace RoussKS\FinancialYear\Tests\Unit\Adapters;
 
+use DateTimeImmutable;
 use RoussKS\FinancialYear\Adapters\AbstractAdapter;
 use RoussKS\FinancialYear\Tests\BaseTestCase;
 use RoussKS\FinancialYear\Adapters\DateTimeAdapter;
@@ -59,5 +60,45 @@ class DateTimeAdapterTest extends BaseTestCase
         $dateTimeAdapter->setFyWeeks(!$fiftyThreeWeeks);
 
         $this->assertNotSame($fyEndDate->format('YmdHis'), $dateTimeAdapter->getFyEndDate()->format('YmdHis'));
+    }
+
+    /**
+     * @test
+     *
+     * @throws \RoussKS\FinancialYear\Exceptions\ConfigException
+     * @throws \RoussKS\FinancialYear\Exceptions\Exception
+     */
+    public function assertGetFyStartDateReturnsDateTimeImmutableObject()
+    {
+        $types = [AbstractAdapter::TYPE_CALENDAR, AbstractAdapter::TYPE_BUSINESS];
+
+        $dateTimeAdapter = new DateTimeAdapter(
+            AbstractAdapter::TYPE_BUSINESS,
+            $this->faker->dateTime,
+            null,
+            array_rand($types,1)
+        );
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $dateTimeAdapter->getFyStartDate());
+    }
+
+    /**
+     * @test
+     *
+     * @throws \RoussKS\FinancialYear\Exceptions\ConfigException
+     * @throws \RoussKS\FinancialYear\Exceptions\Exception
+     */
+    public function assertGetFyEndDateReturnsDateTimeImmutableObject()
+    {
+        $types = [AbstractAdapter::TYPE_CALENDAR, AbstractAdapter::TYPE_BUSINESS];
+
+        $dateTimeAdapter = new DateTimeAdapter(
+            AbstractAdapter::TYPE_BUSINESS,
+            $this->faker->dateTime,
+            null,
+            array_rand($types,1)
+        );
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $dateTimeAdapter->getFyEndDate());
     }
 }
