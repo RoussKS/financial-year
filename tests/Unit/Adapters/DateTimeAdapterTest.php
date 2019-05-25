30e9fe23337f9821490766de8515133ed06a4d38
RoussKS <rkarafylla@gmail.com>
2019-05-25 20:43:02 +0100

    setFyweeks test

--- tests/Unit/Adapters/DateTimeAdapterTest.php
+++ tests/Unit/Adapters/DateTimeAdapterTest.php
@@ -2,7 +2,9 @@
 
 namespace RoussKS\FinancialYear\Tests\Unit\Adapters;
 
+use RoussKS\FinancialYear\Adapters\AbstractAdapter;
 use RoussKS\FinancialYear\Tests\BaseTestCase;
+use RoussKS\FinancialYear\Adapters\DateTimeAdapter;
 
 /**
  * Class DateTimeAdapterTest
@@ -11,5 +13,51 @@ use RoussKS\FinancialYear\Tests\BaseTestCase;
  */
 class DateTimeAdapterTest extends BaseTestCase
 {
+    /**
+     * @test
+     *
+     * @throws \RoussKS\FinancialYear\Exceptions\ConfigException
+     * @throws \RoussKS\FinancialYear\Exceptions\Exception
+     */
+    public function settingSameFyWeeksSetsWeeksWithoutChangingEndDateForBusinessType()
+    {
+        $fiftyThreeWeeks = $this->faker->boolean;
 
+        $dateTimeAdapter = new DateTimeAdapter(
+            AbstractAdapter::TYPE_BUSINESS,
+            $this->faker->dateTime,
+            null,
+            $fiftyThreeWeeks
+        );
+
+        $fyEndDate = $dateTimeAdapter->getFyEndDate();
+
+        $dateTimeAdapter->setFyWeeks($fiftyThreeWeeks);
+
+        $this->assertSame($fyEndDate->format('YmdHis'), $dateTimeAdapter->getFyEndDate()->format('YmdHis'));
+    }
+
+    /**
+     * @test
+     *
+     * @throws \RoussKS\FinancialYear\Exceptions\ConfigException
+     * @throws \RoussKS\FinancialYear\Exceptions\Exception
+     */
+    public function settingDifferentFyWeeksSetsWeeksWithDifferentEndDateForBusinessType()
+    {
+        $fiftyThreeWeeks = $this->faker->boolean;
+
+        $dateTimeAdapter = new DateTimeAdapter(
+            AbstractAdapter::TYPE_BUSINESS,
+            $this->faker->dateTime,
+            null,
+            $fiftyThreeWeeks
+        );
+
+        $fyEndDate = $dateTimeAdapter->getFyEndDate();
+
+        $dateTimeAdapter->setFyWeeks(!$fiftyThreeWeeks);
+
+        $this->assertNotSame($fyEndDate->format('YmdHis'), $dateTimeAdapter->getFyEndDate()->format('YmdHis'));
+    }
 }
