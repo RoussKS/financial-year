07a6e6baf3d21b172e9bd7034ef6258fc2e3cefd
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-02-10 19:13:32 +0000

    remove redundant docblocks from interface

--- src/AdapterInterface.php
+++ src/AdapterInterface.php
@@ -12,22 +12,16 @@ interface AdapterInterface
 {
     /**
      * Get the financial year type.
-     *
-     * @return string
      */
     public function getType(): string;
 
     /**
      * Get the number of weeks for business type financial year or null for calendar type.
-     *
-     * @return int|null
      */
     public function getFyWeeks(): ?int;
 
     /**
      * Get the number of periods of the financial year.
-     *
-     * @return int
      */
     public function getFyPeriods(): int;
 
@@ -37,18 +31,12 @@ interface AdapterInterface
      * Only applies to business financial year type and will be set either 52 or 53.
      * Throw ConfigException for calendar type.
      *
-     * @param  bool $fiftyThreeWeeks
-     *
-     * @return void
-     *
      * @throws ConfigException
      */
     public function setFyWeeks(bool $fiftyThreeWeeks = false): void;
 
     /**
      * Get the financial year start date.
-     *
-     * @return DateTimeInterface
      */
     public function getFyStartDate(): DateTimeInterface;
 
@@ -58,9 +46,7 @@ interface AdapterInterface
      * Expects either string ISO-8601 format 'YYYY-MM-DD'
      * or a date object, same object instance as the adapter's that extends the DateTimeInterface
      *
-     * @param  string|DateTimeInterface $date
-     *
-     * @return void
+     * @param string|DateTimeInterface $date
      *
      * @throws ConfigException
      */
@@ -68,17 +54,11 @@ interface AdapterInterface
 
     /**
      * Get the financial year's end date.
-     *
-     * @return DateTimeInterface
      */
     public function getFyEndDate(): DateTimeInterface;
 
     /**
      * Get the date range of the period with the given id.
-     *
-     * @param  int $id
-     *
-     * @return Traversable
      *
      * @throws ConfigException
      */
@@ -87,27 +67,19 @@ interface AdapterInterface
     /**
      * Get the date range of the business week with the given id.
      *
-     * @param  int $id
-     *
-     * @return Traversable
-     *
      * @throws ConfigException
      */
     public function getBusinessWeekById(int $id): Traversable;
 
     /**
-     * @param  string|DateTimeInterface $date
-     *
-     * @return int
+     * @param string|DateTimeInterface $date
      *
      * @throws ConfigException
      */
     public function getPeriodIdByDate($date): int;
 
     /**
-     * @param  string|DateTimeInterface $date
-     *
-     * @return int
+     * @param string|DateTimeInterface $date
      *
      * @throws ConfigException
      */
@@ -116,20 +88,12 @@ interface AdapterInterface
     /**
      * Get the first date of the period with the given id.
      *
-     * @param  int $id
-     *
-     * @return DateTimeInterface
-     *
      * @throws ConfigException
      */
     public function getFirstDateOfPeriodById(int $id): DateTimeInterface;
 
     /**
      * Get the last date of the period with the given id.
-     *
-     * @param  int $id
-     *
-     * @return DateTimeInterface
      *
      * @throws ConfigException
      */
@@ -138,20 +102,12 @@ interface AdapterInterface
     /**
      * Get the first date of the business week with the given id.
      *
-     * @param  int $id
-     *
-     * @return DateTimeInterface
-     *
      * @throws ConfigException
      */
     public function getFirstDateOfBusinessWeekById(int $id): DateTimeInterface;
 
     /**
      * Get the last date of the business week with the given id.
-     *
-     * @param  int $id
-     *
-     * @return DateTimeInterface
      *
      * @throws ConfigException
      */
@@ -160,20 +116,12 @@ interface AdapterInterface
     /**
      * Get the date range of the first business week of the period with the given id.
      *
-     * @param  int $id
-     *
-     * @return Traversable
-     *
      * @throws ConfigException
      */
     public function getFirstBusinessWeekByPeriodId(int $id): Traversable;
 
     /**
      * Get the date range of the second business week of the period with the given id.
-     *
-     * @param  int $id
-     *
-     * @return Traversable
      *
      * @throws ConfigException
      */
@@ -182,20 +130,12 @@ interface AdapterInterface
     /**
      * Get the date range of the third business week of the period with the given id.
      *
-     * @param  int $id
-     *
-     * @return Traversable
-     *
      * @throws ConfigException
      */
     public function getThirdBusinessWeekOfPeriodId(int $id): Traversable;
 
     /**
      * Get the date range of the fourth business week of the period with the given id.
-     *
-     * @param  int $id
-     *
-     * @return Traversable
      *
      * @throws ConfigException
      */
@@ -204,23 +144,17 @@ interface AdapterInterface
     /**
      * Get the date range of the 53rd business week (if business type financial year & if exists, otherwise Exception).
      *
-     * @return Traversable
-     *
      * @throws ConfigException
      */
     public function getFiftyThirdBusinessWeek(): Traversable;
 
     /**
      * Get the start date of the next financial year.
-     *
-     * @return DateTimeInterface
      */
     public function getNextFyStartDate(): DateTimeInterface;
 
     /**
      * Validate configuration.
-     *
-     * @return void
      *
      * @throws ConfigException
      */
